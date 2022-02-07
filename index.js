2b3f5b53c684fe0624d75070468031d4ee036e82
nicolly.costaa <Nicolly65Costa@hotmail.com>
2022-02-07 15:15:28 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,35 @@
+/* 
+operadores aritméticos
+*  + adição e tbm pode ser Conctenação
+* - = subtração
+* /= subtração 
+*  -*= mutipliação
+*- **= potenciação
+* % resto da divisão 
+*/
+const num1 = 3;
+const num2 = 2;
+const num3 = 5;
+//console.log(num1 + num2 * num3);
+/*
+() primeiro o que esta aqui
+** depois  potenciação
+ / % a divisão e resto 
+ + -
+*/
+// incremntador ++
+// não pode usar o ++ em const pois const nõ pode ser modificado 
+// decrementador --
+
+//const passo = 2;
+let contador = 2;
+contador **= 10;
+//operadores de atribuição 
+//contador -= 2;
+//contador += 2;
+//contador = contador + passo;
+console.log (contador);
+
+// NaN - not a number, parseInt (inteiro), ParseFloat (decimal), Number 
+
+
